5c80f872525d087626028b33fa030834a4caa528
moguanshan99 <QYP999901@163.com>
2019-09-16 13:32:59 +0800

    React后台管理系统--登录完成

---
+++ src/api/request.js
@@ -0,0 +1,91 @@
+/*
+* 1.对请求进行进一步的封装，在发送请求中只处理成功，失败的交给拦截器去操作，简化了请求的操作
+* 2.发送请求的同时会携带一些公共的参数，故需要设置拦截器，用来处理需要发送的一些公共参数
+*/
+
+import axios from 'axios';
+import store from '@redux/store';
+import {message} from 'antd';
+
+//创建axios的实例
+const instance=axios.create({
+        //基本路径
+        baseURL:'http://localhost:3000/api',
+
+        //超时处理
+        timeout:3000
+
+});
+
+//设置请求拦截器---发送请求之前触发函数（目的是为了携带一些公共的请求头参数，eg：token）
+instance.interceptors.request.use(
+
+    (config) =>{
+        //设置需要发送的公共请求头参数
+        //config就是发送请求的配置对象信息（请求的方法，请求头，请求头参数）
+
+        /*
+        * 1.这里需要发送的公共请求头参数就是authorzation，参数值就是token
+        * 2.token值的获取方式有两种
+        *      ----1).从localstorage获取token，但是由于localstorage存储在本地，可近似看成是对数据库进行的操作，因此效率较慢
+        *      ----2).从redux中获取token，redux是内存存储，处理速度较快，故在这里利用redux来获取token
+        * 3.如何获取redux中的token？
+        *      ----1).利用高阶组件connect方法，但是要使用connect的必须是一个组件，这里不是组件故使用不了
+        *      ----2).使用store.getState()方法来获取状态数据
+        * 4./api/login不需要请求头参数要做处理
+        */
+        const {token}=store.getState().user;
+        if (token){
+            config.headers.authorzation=token;
+        }
+       return config;
+}
+    );
+
+//设置响应拦截器---处理响应之前触发的函数
+instance.interceptors.response.use(
+
+
+    /*
+    * 1.请求成功---状态码为200，但是请求成功不代表功能成功
+    * 2.第一个回调相当于一个then方法，返回一个实例触发后面（即触发axios请求中的catch方法）的then方法（如果返回的是一个成功的实例【功能成功】）或者是         catch方法（如果返回的是一个失败的实例【功能失败】）
+    *
+    */
+    (response)=>{
+
+        const result=response.data;
+
+        if (result.status===0){
+            //如果status===0，代表功能成功
+            return result.data;
+        }else{
+            //功能失败---错误信息
+             message.error(result.msg);
+
+            //返回一个失败的Promise实例---触发后面的catch方法
+            return Promise.reject(result.msg)
+        }
+
+    },
+
+    //
+    /*
+    * 1.请求失败---响应状态码为400，500
+    * 2.第二个回调相当于一个catch方法，返回一个失败的实例，触发后面的catch方法（即触发axios请求中的catch方法）
+    *
+    *
+    */
+    (error)=>{
+
+        //打印出错误的结果，方面代码人员调试
+        console.log('axios请求失败：',error);
+
+        //错误信息
+        message.error('未知错误，请联系管理员');
+
+        //返回一个失败的Promise实例---触发后面的catch方法
+        return Promise.reject('未知错误，请联系管理员')
+    }
+);
+
+export default instance;
--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Form,Input,Icon,Button,message} from 'antd'
-import axios from 'axios';
 import {connect} from 'react-redux'
 import {saveUser} from "@redux/action-creators";
 import './index.less'
 import logo from './logo.png';
+import {resLogin} from '../../api'
 
 
 /*
@@ -84,63 +84,80 @@ import logo from './logo.png';
                 *    ----"proxy": "http://localhost:5000"
                 *               ---目标服务器
                 */
-                axios.post('http://localhost:3000/api/login',{username,password})
+                // axios.post('http://localhost:3000/api/login',{username,password})
+                //
+                // //返回的值是一个promise实例，故可以使用.then().catch()
+                //
+                // //解构赋值  {data}=response.data
+                //     .then(({data})=>{
+                //         //请求发送成功
+                //         //判断功能是否完成（即判断返回的status值）
+                //         if (data.status===0){
+                //             message.success('登录成功');
+                //
+                //             /*
+                //             * 登录成功之后需要保存用户的其他数据，eg：token等
+                //             * 1.数据保存在哪里呢？
+                //             *   ---如果数据保存在组件中，那么只有该组件才能用，但是返回的用户数据不仅仅是该组件在使用，其他的组件可能也要用
+                //             *   ---故要将用户数据存储到redux中（redux就是集中管理多个组件的共享数据）
+                //             * 2.redux是内存存储，页面一旦刷新记录就没有啦，因此还要做持久化存储
+                //             *       ---localStorage ：永久存储（只要不删除缓存就一直在）
+                //             *           ---localStorage有三个方法
+                //             *                    1).存储：setItem(key,value)
+                //             *                    2).读取：getItem(key)
+                //             *                    3).删除：removeItem(key)
+                //             *           ---将这些方法封装一下，放在一个专门的文件中——utils（一般用来放封装工具的方法）
+                //             *       ---sessionStorage: 会话存储，一旦页面一关闭，记录就会删除
+                //             * 3.Login组件要使用redux中的action.data
+                //             *       ---在react-redux中用一个方法：connect()(),是一个高阶组件，通过connect方法可以将action-creators中                                       的方法作为props的属性来进行组件间数据的传递
+                //             */
+                //
+                //             this.props.saveUser(data.data);
+                //
+                //             //登录成功跳转'/'路由
+                //             /*
+                //             *  <Redirect to="/"/>;
+                //             *  1.用于在render方法中进行重定向（在组件中使用就单纯的将Redirect看成组件）
+                //             */
+                //
+                //             /*
+                //             *1. Login组件是通过Route组件加载的，故Login组件props中有history等三个属性，可以通过浏览器历史纪录进行跳转
+                //             *2. replace：替换，不需要回退
+                //             *3. push：会在组件后追加一个新的，可回退
+                //             */
+                //             this.props.history.replace('/');  //用于非render方法中进行路由的跳转
+                //         }else{
+                //             message.error(data.msg)
+                //         }
+                //     })
+                //     .catch((error)=>{
+                //         //请求发送失败
+                //         message.error('未知错误，请联系管理员')
+                //     })
+                //
+                //     //当登录失败的时候要清空密码，请求时异步的，代码不能写在请求中，否则当你一提交密码还没有等请求发送密码就已经清空了，Es9提供了新                      的方法finally，不管是成功还是失败都会触发
+                //     .finally(()=>{
+                //         this.props.form.resetFields(['password']);
+                //     })
 
-                //返回的值是一个promise实例，故可以使用.then().catch()
+                     resLogin(username,password)
 
-                //解构赋值  {data}=response.data
-                    .then(({data})=>{
-                        //请求发送成功
-                        //判断功能是否完成（即判断返回的status值）
-                        if (data.status===0){
+                    .then((result)=>{
+
+                           //登录成功
                             message.success('登录成功');
 
-                            /*
-                            * 登录成功之后需要保存用户的其他数据，eg：token等
-                            * 1.数据保存在哪里呢？
-                            *   ---如果数据保存在组件中，那么只有该组件才能用，但是返回的用户数据不仅仅是该组件在使用，其他的组件可能也要用
-                            *   ---故要将用户数据存储到redux中（redux就是集中管理多个组件的共享数据）
-                            * 2.redux是内存存储，页面一旦刷新记录就没有啦，因此还要做持久化存储
-                            *       ---localStorage ：永久存储（只要不删除缓存就一直在）
-                            *           ---localStorage有三个方法
-                            *                    1).存储：setItem(key,value)
-                            *                    2).读取：getItem(key)
-                            *                    3).删除：removeItem(key)
-                            *           ---将这些方法封装一下，放在一个专门的文件中——utils（一般用来放封装工具的方法）
-                            *       ---sessionStorage: 会话存储，一旦页面一关闭，记录就会删除
-                            * 3.Login组件要使用redux中的action.data
-                            *       ---在react-redux中用一个方法：connect()(),是一个高阶组件，通过connect方法可以将action-creators中                                       的方法作为props的属性来进行组件间数据的传递
-                            */
+                            //保存用户数据
+                            this.props.saveUser(result.data);
 
-                            this.props.saveUser(data.data);
+                            //登录成功，跳转到'/'路由
+                            this.props.history.replace('/');  //用于非render方法中进行路由的跳转
 
-                            //登录成功跳转'/'路由
-
-                            /*
-                            *  <Redirect to="/"/>;
-                            *  1.用于在render方法中进行重定向（在组件中使用就单纯的将Redirect看成组件
-                            */
-
-                            /*
-                            *1. Login组件是通过Route组件加载的，故Login组件props中有history等三个属性，可以通过浏览器历史纪录进行跳转
-                            *2. replace：替换，不需要回退
-                            *3. push：会在组件后追加一个新的，可回退
-                            */
-                            this.props.history.replace('/');
-                        }else{
-                            message.error(data.msg)
-                        }
                     })
                     .catch((error)=>{
-                        //请求发送失败
-                        message.error('未知错误，请联系管理员')
-                    })
-
-                    //当登录失败的时候要清空密码，请求时异步的，代码不能写在请求中，否则当你一提交密码就会清空，Es9提供了新的方法finally，不管时成功                     还是失败都会触发
-                    .finally(()=>{
+                       //清空密码
                         this.props.form.resetFields(['password']);
                     })
-
 
             }
         })
